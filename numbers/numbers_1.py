0b306cb94c8938a34f436b5b7ce22aa87d2257fb
Tiago Reis <95048639+tiago-c-reis@users.noreply.github.com>
2022-01-31 15:15:33 +0000

    Create numbers_1.py
    Código desenvolvido durante a aula 'Números em Python - Introdução e Tipos'. Novos exemplos foram adicionados para ajudar na apredizagem de strings em Python. Se tiver qualquer dúvida, envie um comentário no vídeo referente a esta aula: https://youtu.be/nKnkulM6-Kg

---
+++ numbers/numbers_1.py
@@ -0,0 +1,36 @@
+# --- Números em Python - Introdução e Tipos | Vídeo-Aula ---
+#
+# ▶️ Canal YouTube: Automation and Data Science
+#           url: https://www.youtube.com/channel/UCOGLG_-uQco3Z7YGXtbEbEg
+#
+#       👉  video: https://youtu.be/nKnkulM6-Kg
+#
+# 💡 Dica: converta para comentário as linhas de código que não ver representada na consola.
+#          Exemplo: # print(canal)
+#
+# ✔️ Ajude o nosso canal de Educação:
+#       Partilhe, Faça like e Comente.
+#       Ajudar este canal é ajudar toda a comunidade.
+#
+# ---
+
+# 1. Como definir números em Python
+numero = 20
+
+print(numero)
+
+# 2. Definir variáveis do tipo int, float e complex e type()
+val = '5 + 2j'
+
+tipo = type(val)
+
+print(val)
+print(tipo)
+
+# 3. strings e números
+val_1 = 3
+val_2 = '3'
+
+resultado = val_2 * 2    # altere entre val_1 e val_2 para ver a diferença
+
+print(resultado)
